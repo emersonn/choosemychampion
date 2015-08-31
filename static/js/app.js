8e9f2087a7043c8f83d7f7bc992d6fb2e6106774
Emerson Matson <emersonn@cs.washington.edu>
2015-08-30 19:24:08 -0700

    version 2.0. modified whole website. changed website layout, cleaned up directories.

--- static/js/app.js
+++ static/js/app.js
@@ -1,194 +1,126 @@
-    (function() {
-      // todo: clean this up
+(function() {
+  var app = angular.module('league', ['ngRoute', 'ngResource', 'ngAnimate', 'ngMaterial', 'chart.js']);
 
-      // defines the app, takes the Material, Route, Resource, and Animate dependencies
-      var app = angular.module('league', ['ngMaterial', 'ngRoute', 'ngResource', 'ngAnimate']);
+  // TODO: use reverse URLs here
+  app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: '/static/landing.html'
+      }).
+      when('/summoner/:playerName/:locationName', {
+        controller: 'ChampionController',
+        templateUrl: '/static/summoner.html'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
 
-      // configures the app, mostly used to configure routes
-      // routes are used to keep the user in the same page without reloads
-      app.config(['$routeProvider', '$httpProvider', '$mdThemingProvider', function($routeProvider, $httpProvider, $mdThemingProvider) {
-        $routeProvider.when('/', {
-          controller: 'SummonerController',
-          templateUrl: '/static/main.html'
-        }).when('/summoner/:summonerName', {
-          controller: 'ChampionsController',
-          templateUrl: '/static/champions.html'
-        }).otherwise({
-          redirectTo: '/'
+  app.controller('ChampionController', [
+      '$routeParams',
+      'Champion',
+      '$scope',
+      'ChampionDetail',
+      '$timeout',
+  function($routeParams, Champion, $scope, ChampionDetail, $timeout) {
+    $scope.playerName = $routeParams.playerName;
+    $scope.summoner_stats = false;
+
+    $scope.TOP = "TOP";
+    $scope.MIDDLE = "MIDDLE";
+    $scope.BOTTOM = "BOTTOM";
+    $scope.JUNGLE = "JUNGLE";
+
+    $scope.getChampionData = function(champion) {
+      if ($scope[champion.championId] == false || $scope[champion.championId] == undefined) {
+        console.log(champion.championId + " (" + champion.role + ") requested for more data...");
+
+        ChampionDetail.query({champion_id: champion.championId, role: champion.role}, function(data) {
+          console.log("Got detailed champion data...");
+          $scope[champion.championId] = true;
+
+          $scope[champion.championId + 'Counters'] = data.counters;
+          $scope[champion.championId + 'Assists'] = data.assists;
+
+          $timeout(function() {
+            $scope[champion.championId + 'Data'] = [data.days_seen.data];
+            $scope[champion.championId + 'Labels'] = data.days_seen.labels;
+
+            $scope[champion.championId + 'WonData'] = [data.days_won.data];
+            $scope[champion.championId + 'WonLabels'] = data.days_won.labels;
+          }, 500);
         });
+      } else {
+        $scope[champion.championId] = false;
+      }
+    }
 
-        // defines the color palette for the material themes
-        $mdThemingProvider.theme('default')
-          .primaryPalette('cyan')
-          .accentPalette('amber');
-      }]);
+    $scope.champions = Champion.query({username: $scope.playerName, location: $routeParams.locationName}, function(){
+      console.log("Got champions...");
+      $scope.summoner_stats = true;
+    }, function() {
+      console.log("Error in getting champions.");
+    });
+  }]);
 
-      // main page controller. sets the playername and when the player name is received it
-      // redirects the user to the respective summoner
-      app.controller("SummonerController", ['$http', '$location', '$scope', 'Stats', function($http, $location, $scope, Stats) {
-        this.playerName = "";
+  // TODO: use reverse URLs here
+  app.controller('SummonerController', ['$scope', '$location', function($scope, $location) {
+    $scope.locationName = "na";
 
-        $scope.stats = Stats.get();
+    $scope.getSummoner = function() {
+      if ($scope.playerName != undefined) {
+        console.log("Redirecting user to summoner page...");
+        $location.path("/summoner/" + $scope.playerName + "/" + $scope.locationName);
+      }
+    }
+  }]);
 
-        this.getSummoner = function() {
-          console.log("Getting summoner " + this.playerName);
-          $location.path('/summoner/' + this.playerName);
-        }
-      }]);
+  app.controller('StatsController', ['$scope', 'Numbers', function($scope, Numbers) {
+    $scope.initial_stats = false;
 
-      app.controller("RandomController", ['$http', '$scope', 'RandomStats', function($http, $scope, RandomStats) {
-        $scope.stats = RandomStats.get();
-      }]);
+    Numbers.get(function(data) {
+      console.log("Successfully got stats.");
 
-      // the champions controller. requests the champion data from the server
-      // and routes it to the template for display
-      app.controller("ChampionsController", ['$http', '$location', '$routeParams', 'SessionService', 'Champion', '$scope', 'ChampionReset',
-        function($http, $location, $routeParams, SessionService, Champion, $scope, ChampionReset) {
-          $scope.playerName = "";
-          $scope.playerName = $routeParams.summonerName;
+      $scope.initial_stats = true;
+      $scope.stats = data.stats;
+      $scope.winning_champ = data.winning_champ;
 
-          $scope.playerId = 0;
+      $scope.picks_data = [data.champion_picks.data];
+      $scope.picks_labels = data.champion_picks.labels;
+      $scope.picks_images = data.champion_picks.images;
 
-          // storage for all the data
-          $scope.champions = {};
+      $scope.winning_champ_data = data.winning_champ.role_distribution.data;
+      $scope.winning_champ_labels = data.winning_champ.role_distribution.labels;
+      $scope.winning_champ_assists = data.winning_champ.assists;
+    });
+  }]);
 
-          // used for the tabs
-          $scope.data = {
-            selectedIndex: 0,
-          };
+  app.directive("filteredChamps", function() {
+    return {
+      restrict: "E",
+      templateUrl: "/static/championList.html",
+      scope: {
+        certainFilter: '='
+      }
+    };
+  });
 
-          $scope.TOP = "TOP";
-          $scope.MIDDLE = "MIDDLE";
-          $scope.BOTTOM = "BOTTOM";
-          $scope.JUNGLE = "JUNGLE";
+  app.factory('Champion', ['$resource', function($resource) {
+    return $resource('/api/champions/:username/:location',
+      {username: '@username', location: '@location'},
+      {query: {method: 'GET', params: {username: '@username', location: '@location'}}});
+  }]);
 
-          // used for template display of particular data messages
-          $scope.gettingData = true;
-          $scope.gotError = false;
+  app.factory('ChampionDetail', ['$resource', function($resource) {
+    return $resource('/api/stats/:champion_id/:role',
+      {champion_id: '@champion_id', role: '@role'},
+      {query: {method: 'GET', params: {champion_id: '@champion_id', role: '@role'}}});
+  }]);
 
-          // tab functions
-          $scope.next = function() {
-            $scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 3);
-          };
+  app.factory('Numbers', ['$resource', function($resource) {
+    console.log("Getting stats...");
+    return $resource('/api/numbers/');
+  }]);
 
-          $scope.previous = function() {
-            $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
-          };
-
-          $scope.backToFront = function() {
-            $location.path("/");
-          };
-
-          $scope.resetStats = function() {
-            console.log("Attempting to reset user...");
-            $scope.champions = ChampionReset.query({username: $scope.playerName, userId: $scope.playerId}, function() {
-              console.log("Got player...");
-            });
-          }
-
-          // requests data from the server and stores it in the controller
-          SessionService.StartSession($scope.playerName).then(function(data) {
-            console.log("Yay! Found player " + data['data']['user_id'] + " from " + $scope.summonerName + ".");
-            $scope.playerId = encodeURI(data['data']['user_id']);
-
-            // todo: add handler for retries of the full information and also
-            // giving better error feedback
-            console.log("Now requesting full information...");
-            $scope.champions = Champion.query({username: $scope.playerName, userId: $scope.playerId}, function(){
-              console.log("Finished the query...");
-              $scope.gettingData = false;
-            }, function() {
-              $scope.gotError = true;
-            });
-          }).catch(function() {
-            console.log("Oh no!");
-            $scope.gotError = true;
-          });
-      }]);
-
-      // starts the session for attempting to query player name data
-      app.service('SessionService', ['$http', '$q', '$timeout', function($http, $q, $timeout) {
-        var _this = this;
-        var _maxRetryCount = 5;
-
-        this.StartSession = function(username, retries) {
-          retries = angular.isUndefined(retries) ? _maxRetryCount : retries;
-
-          return $http.get('/api/' + username)
-            .then(function(data) {
-              return data;
-            }, function(error) {
-              if (retries > 0) {
-                console.log("Trying again with " + retries + ".");
-                var defer = $q.defer();
-
-                $timeout(function() {
-                  defer.resolve(_this.StartSession(username, --retries))
-                }, 2500);
-                return defer.promise;
-              }
-              return $q.reject('oops!');
-            });
-        }
-      }]);
-
-      // todo: implementing more responsive data collection. if it doesn't connect
-      // it doesn't give good error information or even retries.
-
-      // the storage unit for storing champion data. calling .query() returns all
-      // of the data to use.
-      app.factory('Champion', ['$resource', function($resource) {
-        // return $resource('/api/stats/:username/:user_id', {}, {
-        //   query: {method: 'GET', isArray: true}
-        // });
-        return $resource('/api/stats/:username/:userId',
-          {username: '@username', userId: '@userId'},
-          {query: {method: 'GET', params: {username: '@username', userId: '@userId'}}});
-      }]);
-
-      app.factory('ChampionReset', ['$resource', function($resource) {
-        return $resource('/api/stats/:username/:userId/reset',
-          {username: '@username', userId: '@userId'},
-          {query: {method: 'GET', params: {username: '@username', userId: '@userId'}}});
-      }]);
-
-      app.factory('Stats', ['$resource', function($resource) {
-        return $resource('/api/internal/stats');
-      }]);
-
-      app.factory('RandomStats', ['$resource', function($resource) {
-        return $resource('/api/internal/stats/random');
-      }]);
-
-      // handles the view for the index
-      app.directive('summonerLogin', function() {
-        return {
-          restrict: 'E',
-          templateUrl: 'main.html'
-        }
-      });
-
-      // handles the view for the champion listings, and allow for refactoring
-      // of the champion list
-      app.directive("filteredChamps", function() {
-        return {
-          restrict: "E",
-          templateUrl: "/static/championList.html",
-          scope: {
-            certainFilter: '='
-          }
-        };
-      });
-
-      // filter for encoding URIs
-      app.filter('encodeURIComponent', function() {
-        return window.encodeURIComponent;
-      });
-
-      app.filter('capitalize', function() {
-        return function(input, scope) {
-          input = input.toLowerCase();
-          return input.substring(0, 1).toUpperCase() + input.substring(1);
-        };
-      });
-    })();
+  $("#summonerName").focus();
+})();

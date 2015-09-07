a8e9d3b91569c6cddeaa73b1201267e5046267a7
Emerson Matson <emersonn@cs.washington.edu>
2015-09-07 11:22:30 -0700

    updated to PEP8

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # API Settings
-API_KEY = "a956dacd-09ed-49d2-a610-dc9597599af3"
+API_KEY = "45501fd5-d2b4-40a4-89c2-86b15b175904"
 URLS = {'ids': 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/',
         'stats': 'https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/%s/ranked',
         'champion': 'https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/',
@@ -9,9 +9,10 @@ URLS = {'ids': 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/',
         'match': 'https://na.api.pvp.net/api/lol/na/v2.2/match/'}
 
 # Database Settings
-DATABASE = 'sqlite:///database/history.db'
+DATABASE = 'mysql://root@localhost/league'
 
 # todo: MemCache?
+
 # Cache Settings
 from werkzeug.contrib.cache import SimpleCache
 CACHE = SimpleCache()

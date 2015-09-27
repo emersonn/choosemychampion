ae95d5d40b0472ed5a72a6f90e388fc679266eac
Emerson Matson <emersonn@cs.washington.edu>
2015-09-26 21:13:16 -0700

    added coverage for all functions

--- tests/tests_riot.py
+++ tests/tests_riot.py
@@ -1,5 +1,5 @@
 import mock
-from nose.tools import set_trace
+# from nose.tools import set_trace
 
 from riot import RiotSession, URLS
 
@@ -45,6 +45,7 @@ class TestRiotSession(object):
             # TODO: What if the featured is actually not None?
             #       Check if it actually made a request?
             #       Then we would be testing Riot's servers instead.
+            #       Maybe check if there is actual request headers?
             # assert self.session.get_featured() is not None
 
     def test_get_matches(self):
@@ -73,4 +74,46 @@ class TestRiotSession(object):
                     match=str(1234)
                 ),
                 params={}
+            )
+
+    def test_get_match_list(self):
+        with mock.patch.object(self.session, "get") as get:
+            get.return_value.json.return_value = {}
+            l = self.session.get_match_list(1234)
+            assert l == []
+
+            get.assert_called_once_with(
+                URLS['match_list'].format(
+                    location=self.session.location,
+                    player=str(1234)
+                ),
+                params={'rankedQueues': 'RANKED_SOLO_5x5'}
+            )
+
+    def test_get_ids(self):
+        with mock.patch.object(self.session, "get") as get:
+            get.return_value.json.return_value = {}
+            ids = self.session.get_ids([])
+            assert ids == {}
+
+            get.assert_called_once_with(
+                URLS['ids'].format(
+                    location=self.session.location,
+                    players=''
+                ),
+                params={}
+            )
+
+    def test_get_champion(self):
+        with mock.patch.object(self.session, "get") as get:
+            get.return_value.json.return_value = {}
+            champion_data = self.session.get_champion(1234)
+            assert champion_data == {}
+
+            get.assert_called_once_with(
+                URLS['champion'].format(
+                    location=self.session.location,
+                    champion=str(1234)
+                ),
+                params={'champData': 'all'}
             )

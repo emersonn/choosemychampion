75e62f9c967c63f81cbb6955e44442139237df4e
Emerson Matson <emersonn@cs.washington.edu>
2015-09-26 20:40:53 -0700

    cleaned up the file and abstracted out URLS

--- riot.py
+++ riot.py
@@ -10,52 +10,64 @@
 
 import requests
 
-# TODO: put this client side. use warnings instead.
-#       unless some type of force_sleep is enabled.
-from time import sleep
+BASE_URL = "https://{location}.api.pvp.net/"
+# https://{location}.api.pvp.net/api/lol/{location}/
+BASE_API_URL = BASE_URL + "api/lol/{location}/"
+# https://{location}.api.pvp.net/api/lol/static-data/{location}/
+BASE_STATIC_URL = BASE_URL + "api/lol/static-data/{location}/"
 
-# TODO: Make base URL
 URLS = {
-    'ids': 'https://{location}.api.pvp.net/api/lol/{location}/v1.4/summoner/by-name/{players}',
-    'stats': 'https://{location}.api.pvp.net/api/lol/{location}/v1.3/stats/by-summoner/{player}/ranked/',
-    'champion': 'https://{location}.api.pvp.net/api/lol/static-data/{location}/v1.2/champion/{champion}/',
+    'ids': BASE_API_URL + 'v1.4/summoner/by-name/{players}',
+    'stats': BASE_API_URL + 'v1.3/stats/by-summoner/{player}/ranked/',
+    'champion': BASE_STATIC_URL + 'v1.2/champion/{champion}/',
     'featured': 'https://{location}.api.pvp.net/observer-mode/rest/featured/',
-    'matches': 'https://{location}.api.pvp.net/api/lol/{location}/v2.2/matchhistory/{player}/',
-    'match': 'https://{location}.api.pvp.net/api/lol/{location}/v2.2/match/{match}/',
-    'match_list': 'https://{location}.api.pvp.net/api/lol/{location}/v2.2/matchlist/by-summoner/{player}/'
+    'matches': BASE_API_URL + 'v2.2/matchhistory/{player}/',
+    'match': BASE_API_URL + 'v2.2/match/{match}/',
+    'match_list': BASE_API_URL + 'v2.2/matchlist/by-summoner/{player}/'
 }
 
-# TODO: maybe try abstracting out the games into json.loads
-#       with object_hook to make consistent across the platform,
-#       also allowing for update of the riot API without
-#       changing a lot of code
-
-# TODO: add error checking, error codes, etc. r.status_code = 429
-
-# TODO: rate limiting automatic sleep. only use for this module.
+# TODO: Try abstracting with json.loads in object_hook to make consistent
+# TODO: Add error checking, error codes, etc. r.status_code = 429
+# TODO: Rate limiting throw errors.
+# TODO: Streamline the .get() and .format() and add unit tests for
+#       the requested URL.
 
 
 class RiotSession(requests.Session):
     def __init__(self, api, location="na"):
         super(RiotSession, self).__init__()
         self.params.update({'api_key': api})
-
         self.location = location
 
-    # TODO: KeyError
+    def _get_request(self, connection, formats):
+        """ Builds a get request with the given API request
+
+        Args:
+            connection: Desired URL key in the URLS list.
+            formats: Keyword arguments to format the string with.
+
+        Returns:
+            json: JSON loaded response from the server.
+
+        Assumptions:
+            Desired location for API server is the current Session's stored
+                location.
+        """
+
+        return self.get(URLS[connection].format(**formats)).json()
+
     def get_featured(self):
         try:
             return self.get(URLS['featured'].format(
                 location=self.location
             )).json()['gameList']
         except KeyError:
-            sleep(10)
             return []
 
-    # TODO: r['matches'] exists (KeyError), and even if ValueError
     def get_matches(self, player, matches=5, match_type='RANKED_SOLO_5x5'):
         import warnings
         warnings.warn("Riot will be depricating this URL.")
+
         try:
             return self.get(
                 URLS['matches'].format(
@@ -63,23 +75,16 @@ class RiotSession(requests.Session):
                 ),
                 params={'rankedQueues': match_type, 'endIndex': matches}
             ).json()['matches']
-        except (KeyError, ValueError) as err:
-            sleep(10)
+        except KeyError:
             return []
 
-    # TODO: r['matches'] exists (KeyError), and even if ValueError
     def get_match(self, match):
-        try:
-            return self.get(
-                URLS['match'].format(location=self.location, match=str(match))
-            ).json()
-        except ValueError:
-            sleep(10)
-            return {}
+        return self.get(
+            URLS['match'].format(location=self.location, match=str(match))
+        ).json()
 
     # TODO: UnicodeEncodeError: 'ascii' codec can't encode character u'\xfc'
     #       in position 218: ordinal not in range(128)
-    #       maybe grab a random set from the featured players?
     def get_ids(self, players):
         return self.get(
             URLS['ids'].format(
